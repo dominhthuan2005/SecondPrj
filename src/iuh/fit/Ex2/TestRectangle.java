a606e8a5381d493eed9f777606b8f95653a8c816
Đỗ Minh Thuận <dominhthuan17102005@gmail.com>
2024-08-27 22:34:21 +0700

    Update TestRectangle.java

--- src/iuh/fit/Ex2/TestRectangle.java
+++ src/iuh/fit/Ex2/TestRectangle.java
@@ -1,4 +1,15 @@
+/*
+ * @ Do Minh Thuan.java    1.0 21
+ * Copyright (c) 2024 IUH. All rights reserved
+ *
+ */
 package iuh.fit.Ex2;
+/*
+ * @description:
+ * @author: Minh Thuan
+ * @date: 28/08/2024
+ * @version: 1.0
+ */
 
 public class TestRectangle {
     public static void main(String[] args) {
